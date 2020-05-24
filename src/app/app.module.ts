259b6ed79e6f01c83d89f4562f18c660ed8225d4
Fideon <fsf91@hotmail.com>
2020-05-24 15:57:12 -0500

    Added a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponentComponent } from './server-component/server-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponentComponent
   ],
   imports: [
     BrowserModule,
